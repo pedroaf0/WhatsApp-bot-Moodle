6784680a22b4837222958c93d0df03cb3e410590
pedroaf0 <pedroaf1233@gmail.com>
2021-03-18 23:37:11 -0300

    init

---
+++ index.js
@@ -0,0 +1,73 @@
+const venom = require('venom-bot');
+
+const request = require("request");
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+const baseurl = 'https://moodle.sertao.ifrs.edu.br';
+const username = 'moodle user';
+const password = 'moodle pass';
+
+const login = (baseurl,username,password)=> new Promise((re,err)=>{
+    request(`${baseurl}/login/token.php?username=${username}&password=${password}&service=moodle_mobile_app`, function (error, response, body) {
+        console.log(JSON.parse(body))
+        re(JSON.parse(body))
+    })
+})
+
+const core_calendar_get_calendar_upcoming_view = (baseurl,token)=> new Promise((re,err)=>{
+    request(`${baseurl}/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_calendar_get_calendar_upcoming_view&moodlewssettingfilter=true&moodlewssettingfileurl=true&wstoken=${token}`, function (error, response, body) {
+       // console.log(JSON.parse(body))
+        re(JSON.parse(body))
+    })
+})
+
+function dateformat(int){
+  var data = new Date(int),
+      dia  = data.getDate().toString(),
+      diaF = (dia.length == 1) ? '0'+dia : dia,
+      mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+      mesF = (mes.length == 1) ? '0'+mes : mes,
+      hr  = (data.getHours()).toString(), //+1 pois no getMonth Janeiro começa com zero.
+      hrf = (hr.length == 1) ? '0'+hr : hr,
+      mim  = (data.getMinutes()).toString(), //+1 pois no getMonth Janeiro começa com zero.
+      mimf = (mim.length == 1) ? '0'+mim : mim
+
+      
+  return {diaF, mesF, hrf, mimf }
+}
+
+async function a(){
+  const { token, privatetoken } = await login(baseurl, username, password)
+  
+
+venom
+  .create()
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage(async(message) => {
+    console.log(message)
+    if (message.body === '!atividades') {
+      const events = await core_calendar_get_calendar_upcoming_view(baseurl, token)
+      var _message = '';
+      for (let index = 0; index < events.events.length; index++) {
+        var data = dateformat(events.events[index].timesort*1000)
+        _message = `${_message} [${data.diaF}/${data.mesF} às ${data.hrf}:${data.mimf}] ${events.events[index].name} de ${events.events[index].course.fullname}\n\n`;
+      }
+        console.log(_message)
+
+      client
+        .sendText(message.from, _message)
+        .then((result) => {
+         // console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
+}a()
